2c70ce5e5452fd413bc30ef6f69037d9b1fcabfa
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2023-05-09 19:34:40 +0100

    Uncompleted restaurant-landing page
    I victory build this . its doesn't have the contact and footer section  yet due to my  technical and capability limits.
    thank you for the opportunity will redo it again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./victory.html"],
   theme: {
     screens: {
       sm: "480px",
@@ -10,13 +10,19 @@ module.exports = {
     },
     extend: {
       colors: {
-        'darkGreen': '#387546',
-        'lightGreen': '#598464',
-        'darkText': '#383833',
-        'orange': '#F19A33',
-        'veryLightGreen': '#DEF0D8',
-        'lemonGreen': '#B2C9A3',
-        'gray': '#00000029'
+        
+        someBrown : 'rgba(56, 56, 51, 1)',
+        someYellow : 'rgba(241, 154, 51, 1)',
+        fafafa : "#FAFAFA",
+        color_f19a33 : "#F19A33",
+        color_b2c9a3 :"#B2C9A3",
+        color_387546 : "#387546",
+        color_ffffff :"#FFFFFF",
+        color_383833 : "#383833",
+        color_598463 : "#598463"
+        
+     
+
       },
     },
   },
